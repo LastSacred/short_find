dbe5f74b038d44ef79030b96241a2103bf4aad2f
Arthur Torres <arthurtorres002@gmail.com>
2019-01-09 13:01:42 -0500

    remove short_find-1.0.7.gem

--- spec/short_find_spec.rb
+++ spec/short_find_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe ShortFind do
     expect(ShortFind::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  @array = ["one", "two", "three"]
+  @hash = {"one" => "onev", :two => "twov", :three => "threev"}
+
+  describe ".in_array" do
+    expect("1").to eq "1"
   end
+
+
 end
---
+++ test.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require_relative "lib/short_find.rb"
+
+array = ["one", "two", "three", "four"]
+
+
+
+
+binding.pry
